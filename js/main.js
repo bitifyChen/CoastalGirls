7f27a99c927587885724c955c3c09416ae0ac589
bitifyChen <43808882+bitifyChen@users.noreply.github.com>
2022-01-05 14:30:29 +0800

    22.01.05-bug fix

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-//Nav JS
+//---【Base】 ---
+//Nav
 const menuBtn = document.querySelector("nav > button.menu");
 const menu = document.querySelector("nav");
 menuBtn.addEventListener("click", () => {
@@ -67,6 +68,34 @@ modals.forEach((modal) => {
   });
 });
 
+ //world info display to
+ const worldBtns = document.querySelectorAll(".modal-sidebar>ul>li");
+ const worldInfos = document.querySelectorAll(".modal-info");
+ worldBtns.forEach((worldBtn, index) => {
+   worldBtn.addEventListener("click", () => {
+     worldBtns.forEach((worldBtn) => worldBtn.classList.remove("active"));
+     worldBtn.classList.add("active");
+     showInfo(index);
+   });
+ });
+
+ function showInfo(btnIndex) {
+   worldInfos.forEach((worldInfo, index) => {
+     if (index === btnIndex) {
+       worldInfo.classList.add("active");
+       modalSidebar.classList.remove("open");
+     } else {
+       worldInfo.classList.remove("active");
+     }
+   });
+ }
+
+ const modalMenuBtn = document.querySelector("button.modal-menu");
+ const modalSidebar = document.querySelector(".modal-sidebar");
+ modalMenuBtn.addEventListener("click", () => {
+   modalSidebar.classList.toggle("open");
+ });
+
 // filter Main
 const filterBtns = document.querySelectorAll(".main-btns li");
 const items = document.querySelectorAll(".items a");
@@ -78,8 +107,7 @@ filterBtns.forEach((filterBtn) => {
       //shop
       if (filterBtn.dataset.page === "shop") {
         shopDisplay(filterBtn.dataset.filter);
-      }
-      else{
+      } else {
         itemDisplay(filterBtn.dataset.filter);
       }
     } else {
@@ -88,15 +116,14 @@ filterBtns.forEach((filterBtn) => {
   });
 });
 
-function filterBtnsSet(thisBtn){
-  
-  //Clear active 
-  filterBtns.forEach((filterBtn)=>{
-    filterBtn.classList.remove('active')
-  })
+function filterBtnsSet(thisBtn) {
+  //Clear active
+  filterBtns.forEach((filterBtn) => {
+    filterBtn.classList.remove("active");
+  });
 
   //Set active
-  thisBtn.classList.add('active');
+  thisBtn.classList.add("active");
 }
 
 function itemDisplay(filter) {
@@ -104,11 +131,11 @@ function itemDisplay(filter) {
   if (filter === "*") {
     items.forEach((item) => {
       item.classList.remove("hide");
-      item.querySelector('.item-filter').style.display="block";
+      item.querySelector(".item-filter").style.display = "block";
     });
   } else {
     items.forEach((item) => {
-      item.querySelector('.item-filter').style.display="none";
+      item.querySelector(".item-filter").style.display = "none";
       if (item.classList.contains(filter)) {
         item.classList.remove("hide");
       } else {
@@ -118,7 +145,42 @@ function itemDisplay(filter) {
   }
 }
 
-//shop custom
+//---【Index__news-section】 ---
+const indexNewsBtns = document.querySelectorAll("#indexNews ul.nav li");
+const indexNewsContents = document.querySelectorAll("#indexNews ul.news-in-tab");
+function indexNewsBtnSet(btn) {
+  //Clear active
+  indexNewsBtns.forEach((btn) => {
+    btn.classList.remove("active");
+  });
+
+  //Set active
+  btn.classList.add("active");
+}
+
+function indexNewsContentSet(name){
+   indexNewsContents.forEach((content)=>{
+    if(content.getAttribute('data-name') == name){
+      //Set active
+      content.classList.add('active')
+    }else{
+        //Clear active
+      content.classList.remove('active')
+    }   
+  })
+
+}
+
+indexNewsBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    indexNewsBtnSet(btn)
+    indexNewsContentSet(btn.getAttribute('data-filter'))
+  });
+});
+
+
+
+//---【Shop__】 ---
 const shopPageContent = document.querySelector(".shop-page-content");
 const shopPageItems = document.querySelector(".items-main .items");
 
@@ -126,8 +188,7 @@ function shopDisplay(filter) {
   if (filter === "shop-page-content") {
     shopPageContent.classList.remove("display-none");
     shopPageItems.classList.add("display-none");
-  } 
-  else {
+  } else {
     shopPageContent.classList.add("display-none");
     shopPageItems.classList.remove("display-none");
     if (filter === "*") {
